eaebfdf7c32fdcbc1b391f804e5500589973b120
nacho mazzoni <nachomazzoni1gmail.com>
2023-01-24 18:21:07 -0300

    actualizacion

---
+++ js/funciones.js
@@ -0,0 +1,8 @@
+function mostrar(){
+    document.getElementById('Mostrar').style.display ='block';
+    document.getElementById('boton').style.display='none';
+}
+function ocultar(){
+    document.getElementById('Mostrar').style.display ='none';
+    document.getElementById('boton').style.display='block';
+}
---
+++ js/function.js
@@ -0,0 +1,8 @@
+console.log("hola mundo");
+
+let numero1 = 2;
+let numero2 = 3;
+
+let resultado= numero1+numero2;
+
+console.log("la suma de los numeros es"+ resultado);
